56e4570b18d7e84bb338e42b7a87456370c9fece
kimmo <hemakim0215@naver.com>
2025-10-04 19:13:35 +0900

    .

--- SwiftLog/src/console.rs
+++ SwiftLog/src/console.rs
@@ -1,1 +1,31 @@
 
+
+
+pub struct ConsoleMonitor
+{
+    // Add fields as necessary
+    
+}
+
+impl ConsoleMonitor
+{
+    pub fn new() -> Self
+    {
+        ConsoleMonitor {
+            // Initialize fields
+        }
+    }
+
+    pub fn start(&self)
+    {
+        // Start monitoring console output
+    }
+
+    pub fn do_command_process(&mut self)
+    {
+        // Process commands
+    }
+
+}
+
+
