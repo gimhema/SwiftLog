9d8cbb123f9aa7609d587dfca58d53a976605c81
kimmo <hemakim0215@naver.com>
2025-10-04 19:26:33 +0900

    .

--- SwiftLog/src/console.rs
+++ SwiftLog/src/console.rs
@@ -19,11 +19,35 @@ impl ConsoleMonitor
     pub fn start(&self)
     {
         // Start monitoring console output
+
+        // wait input command in loop
+
+        // on command, call do_command_process
+
+        self.command_loop();
     }
 
-    pub fn do_command_process(&mut self)
+    pub fn command_loop(&self)
+    {
+        // Loop to read commands from console
+        loop {
+
+            // wait input command
+            
+        }
+    }
+
+    pub fn do_command_process(&mut self, command: ConsoleCommand)
     {
         // Process commands
+        match command {
+            ConsoleCommand::Home => self.command_func_home(),
+            ConsoleCommand::Help => self.command_fun_help(),
+            ConsoleCommand::ShowLogList => self.command_func_show_log_list(),
+            ConsoleCommand::SelectLog => self.command_func_select_log(),
+            ConsoleCommand::ClearScreen => self.command_func_clear_screen(),
+            ConsoleCommand::BackupLog => self.command_func_backup_log(),
+        }
     }
 
 }
--- SwiftLog/src/console_command.rs
+++ SwiftLog/src/console_command.rs
@@ -1,4 +1,4 @@
-
+use crate::console::*;
 
 
 
@@ -8,4 +8,32 @@ pub enum ConsoleCommand
     Help,
     ShowLogList,
     SelectLog,
+    ClearScreen,
+    BackupLog,
 }
+
+impl ConsoleMonitor {
+    pub fn command_func_home(&mut self) {
+        // Go to Main Menu
+    }
+
+    pub fn command_fun_help(&mut self) {
+        // Show Help
+    }
+    
+    pub fn command_func_show_log_list(&mut self) {
+        // Show Log List
+    }
+
+    pub fn command_func_select_log(&mut self) {
+        // Select Log
+    }
+
+    pub fn command_func_clear_screen(&mut self) {
+        // Clear Screen
+    }
+
+    pub fn command_func_backup_log(&mut self) {
+        // Backup Log
+    }
+}       
