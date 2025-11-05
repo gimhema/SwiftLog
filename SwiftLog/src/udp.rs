257943ca99b7ee99b55a7bfe56c49c631452f4dd
Kimmo <49871739+gimhema@users.noreply.github.com>
2025-11-05 13:44:02 +0900

    Clean up commented-out packet count functions
    Removed commented-out code related to packet counts.

--- SwiftLog/src/udp.rs
+++ SwiftLog/src/udp.rs
@@ -2,17 +2,8 @@ use std::io;
 use std::net::UdpSocket;
 use std::sync::atomic::{AtomicU64, Ordering};
 
-// pub static RECV_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
-pub static RECV_NORMAL_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
-// pub static ERROR_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
-// pub static WOULD_BACK_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
 
-// pub fn increase_count() {
-//     RECV_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
-// }
-// pub fn get_count() -> u64 {
-//     RECV_PACKET_COUNT.load(Ordering::Relaxed)
-// }
+pub static RECV_NORMAL_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
 
 pub fn increase_normal_count() {
     RECV_NORMAL_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
@@ -21,19 +12,6 @@ pub fn get_normal_count() -> u64 {
     RECV_NORMAL_PACKET_COUNT.load(Ordering::Relaxed)
 }
 
-// pub fn increase_err_count() {
-//     ERROR_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
-// }
-// pub fn get_err_count() -> u64 {
-//     ERROR_PACKET_COUNT.load(Ordering::Relaxed)
-// }
-
-// pub fn increase_would_back_count() {
-//     WOULD_BACK_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
-// }
-// pub fn get_would_back_count() -> u64 {
-//     WOULD_BACK_PACKET_COUNT.load(Ordering::Relaxed)
-// }
 
 pub struct UdpRx {
     sock: UdpSocket,
@@ -59,10 +37,10 @@ impl UdpRx {
                  let _ = on_batch(&self.buf[..n]); 
                 }
             Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
-//                increase_would_back_count();
+
             }
             Err(_e) => {
-                // increase_err_count();
+
             }
         }
     }
