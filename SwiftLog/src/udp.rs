ded540de8e6635b1d6704a4ec738f126cd5815bb
Kimmo <49871739+gimhema@users.noreply.github.com>
2025-11-05 13:43:40 +0900

    Update udp.rs

--- SwiftLog/src/udp.rs
+++ SwiftLog/src/udp.rs
@@ -1,5 +1,39 @@
 use std::io;
 use std::net::UdpSocket;
+use std::sync::atomic::{AtomicU64, Ordering};
+
+// pub static RECV_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
+pub static RECV_NORMAL_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
+// pub static ERROR_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
+// pub static WOULD_BACK_PACKET_COUNT: AtomicU64 = AtomicU64::new(0);
+
+// pub fn increase_count() {
+//     RECV_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
+// }
+// pub fn get_count() -> u64 {
+//     RECV_PACKET_COUNT.load(Ordering::Relaxed)
+// }
+
+pub fn increase_normal_count() {
+    RECV_NORMAL_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
+}
+pub fn get_normal_count() -> u64 {
+    RECV_NORMAL_PACKET_COUNT.load(Ordering::Relaxed)
+}
+
+// pub fn increase_err_count() {
+//     ERROR_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
+// }
+// pub fn get_err_count() -> u64 {
+//     ERROR_PACKET_COUNT.load(Ordering::Relaxed)
+// }
+
+// pub fn increase_would_back_count() {
+//     WOULD_BACK_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
+// }
+// pub fn get_would_back_count() -> u64 {
+//     WOULD_BACK_PACKET_COUNT.load(Ordering::Relaxed)
+// }
 
 pub struct UdpRx {
     sock: UdpSocket,
@@ -15,10 +49,21 @@ impl UdpRx {
 
     // 논블로킹 단발 수신 한 번만 시도 (없으면 WouldBlock)
     pub fn recv_once<F: FnMut(&[u8]) -> io::Result<()>>(&mut self, mut on_batch: F) {
+
+        // RECV_PACKET_COUNT.fetch_add(1, Ordering::Relaxed);
+
+
         match self.sock.recv(&mut self.buf) {
-            Ok(n) => { let _ = on_batch(&self.buf[..n]); }
-            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
-            Err(_e) => {}
+            Ok(n) => {
+                increase_normal_count();
+                 let _ = on_batch(&self.buf[..n]); 
+                }
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+//                increase_would_back_count();
+            }
+            Err(_e) => {
+                // increase_err_count();
+            }
         }
     }
 }
